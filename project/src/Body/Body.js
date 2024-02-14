b061c1f884925048745cd3ede6166189613ab175
Santam Kumai <santtamk@gmail.com>
2024-02-14 21:59:59 +0530

    minor changes

--- project/src/Body/Body.js
+++ project/src/Body/Body.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../Card/Card'
-// import data from '/apidata.json'
+import './bodystyles.css'
 
 const Body = () => {
 
 const [images, setImages] = useState([])
+const [click, setClicked] = useState(false)
 
 useEffect(()=> {
   const fetchImage = async() =>{
     try{
       const response = await fetch("/apidata.json")
       const data = await response.json()
-      console.log(data)
+      // console.log(data)
       setImages(data)
     }catch(err){
       console.error(err)
@@ -20,12 +21,24 @@ useEffect(()=> {
   fetchImage()
 },[])
 
+// const recorderdClick = () => {
+//   setClicked(true);
+// }
+
+useEffect(() => {
+  const shuffleCard = () => {
+    // when we click on card it runs Math.random the map so that the alignment changes.
+      // let id = 0 + Math.floor(Math.random()*(10 - 0 + 1))
+      setImages(data(0 + Math.floor(Math.random()*(10 - 0 + 1))))
+    }
+},[])
+
     
   return (
-    <div>
+    <div className='body'>
       {images.map((image) => (
-        <div key={image.id}>
-          <Card image={image.src} name={image.name}/>
+          <div key={image.id}>
+            <Card image={image.src} name={image.name}/>
         </div>
       ))}
       </div>
@@ -33,3 +46,5 @@ useEffect(()=> {
 }
 
 export default Body
+
+// on click of the card that it rearranges randomly 
--- project/src/Card/Card.js
+++ project/src/Card/Card.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './cardstyles.css'
+import React from "react";
+import "./cardstyles.css";
 
 const Card = (props) => {
   return (
     <div>
-        <figure>
-            <img src={props.image} alt={props.name}/>
-        <figcaption>{props.name}</figcaption>
-        </figure>
+      <figure>
+        <img src={props.image} alt={props.name} />
+        {/* <figcaption>{props.name}</figcaption> */}
+      </figure>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
